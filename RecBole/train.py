8a1e73c52fa1f506ef79ea9779d1291a1f0f3a94
jaeeun <jjjaeunn@gmail.com>
2024-11-21 10:51:04 +0900

    fix: 함수 위치 및 arg 변경 #20

--- RecBole/train.py
+++ RecBole/train.py
@@ -5,15 +5,10 @@ from datetime import datetime
 
 from recbole.config import Config
 from recbole.trainer import Trainer
-from recbole.data import create_dataset, data_preparation
 
-from dataloader import generate_data
+from dataloader import generate_data, get_data
 
-def get_data(config):
-    dataset = create_dataset(config)
-    train_data, valid_data, test_data = data_preparation(config=config, dataset=dataset)
-    return train_data, valid_data, test_data
-
+# checkpoint 생성 시 모델 학습 시각 기록을 위한 함수
 def get_time():
     now = datetime.now()
     # 월, 일, 시간 추출
@@ -24,34 +19,45 @@ def get_time():
     
     return f"{month}-{day}_{hour}-{minute}"
 
-def train(config, train_data):
+# train 함수에서 Logger, Wandb dev 브랜치 버전으로 업데이트 예정
+def train(config):
+    tr_data, val_data, te_data = get_data(config)
     model = model_class(config, tr_data.dataset).to(config['device'])
     trainer = Trainer(config,model)
     print("==== train start ====")
-    best_valid_score, best_valid_result = trainer.fit(tr_data)
+    best_valid_score, best_valid_result = trainer.fit(train_data=tr_data,valid_data=val_data)
+    print("==== train end! ====")
+    print(f"best valid score & result: {best_valid_score}, {best_valid_result}")
+    print("==== evaluation start ====")
+    test_result = trainer.evaluate(te_data)
+    print(f"test_result")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     # model의 종류를 정의합니다 (general, sequential, context-aware)
-    arg( "--type",
+    arg("--type",
         "-t",
         type=str,
         choices=['g','s','c'],
         help="type of recommender model(g:general, s:sequential, c:context-aware)"
     )
-    arg( "--model",
+    arg("--model",
         "-m",
         type=str,
         help="name of recommender"
+    )
+    arg("--data_path",
+        "-d",
+        type=str,
+        help="path of saved data",
+        default='./data/'
     )
     arg("--cur",
         default=get_time(),
         help="current date and time as string")
     args = parser.parse_args()
-    
-    generate_data(args=args)
 
     model_type = {'g':'general_recommender','s':'sequential_recommender','c':'context_aware_recommender'}
     recbole_model = importlib.import_module('recbole.model.'+model_type.get(args.type))
@@ -62,5 +68,5 @@ if __name__ == "__main__":
         config['checkpoint_dir']+args.model+args.cur
     )
 
-    tr_data, val_data, te_data = get_data(config)
-    train(config=config, train_data=tr_data)
+    generate_data(args=args, config=config)
+    train(config=config)
