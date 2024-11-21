ab13512f3b66674985117b7f735eb0017c59cfbf
choitaesoon <cts0817@naver.com>
2024-11-21 16:00:28 +0900

    feat 28: Add EASE loss function #28

--- src/loss/loss_fn.py
+++ src/loss/loss_fn.py
@@ -25,3 +25,28 @@ def multivae_loss(x, output, anneal=1.0):
 
 
 
+def ease_loss(R, R_hat, U, V, lambda_reg=0.01):
+    """
+    EASE loss function with regularization.
+    
+    Parameters:
+    - R: 실제 사용자-아이템 상호작용 행렬 (ground truth)
+    - R_hat: 예측된 사용자-아이템 상호작용 행렬
+    - U: 사용자 특징 행렬
+    - V: 아이템 특징 행렬
+    - lambda_reg: 정규화 계수
+    
+    Returns:
+    - loss: 최종 손실값
+    """
+    # MSE Loss (실제 값과 예측값의 차이)
+    mse_loss = np.mean((R - R_hat) ** 2)
+    
+    # Regularization (L2 regularization)
+    reg_loss = lambda_reg * (np.sum(U ** 2) + np.sum(V ** 2))
+    
+    # Total Loss
+    loss = mse_loss + reg_loss
+    return loss
+
+
