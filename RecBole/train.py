f9916768de7e12077337186bd490b48e1136cf76
jaeeun <jjjaeunn@gmail.com>
2024-11-21 21:24:38 +0900

    refactor: dev 브랜치와 병합을 위한 수정

--- RecBole/train.py
+++ RecBole/train.py
@@ -1,11 +1,14 @@
 import argparse
 import importlib
 import os
+from omegaconf import OmegaConf
 from datetime import datetime
 
 from recbole.config import Config
 from recbole.trainer import Trainer
-
+import sys
+sys.path.insert(0, "/data/ephemeral/home")
+from src.utils.util import Logger, Setting
 from dataloader import generate_data, get_data
 
 # checkpoint 생성 시 모델 학습 시각 기록을 위한 함수
@@ -19,24 +22,42 @@ def get_time():
     
     return f"{month}-{day}_{hour}-{minute}"
 
-# train 함수에서 Logger, Wandb dev 브랜치 버전으로 업데이트 예정
-def train(config):
+def train(config,logger):
+    ### data initialize
     tr_data, val_data, te_data = get_data(config)
+    
+    ### model initialize
     model = model_class(config, tr_data.dataset).to(config['device'])
     trainer = Trainer(config,model)
+
     print("==== train start ====")
     best_valid_score, best_valid_result = trainer.fit(train_data=tr_data,valid_data=val_data)
+
     print("==== train end! ====")
+    logger.info(f"best valid score: {best_valid_score}")
+    logger.info(f"best valid result: {best_valid_result}")
     print(f"best valid score & result: {best_valid_score}, {best_valid_result}")
+    
     print("==== evaluation start ====")
     test_result = trainer.evaluate(te_data)
-    print(f"test_result")
+    logger.info(f"test result: {test_result}")
+    print(f"test_result: {test_result}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     # model의 종류를 정의합니다 (general, sequential, context-aware)
+    arg("--config", 
+        "-c",
+        help='Configuration 파일을 설정합니다.',
+        default='../configs/config.yaml'
+    )
+    arg("--root_dir",
+        "-r",
+        type=str,
+        default='/data/ephemeral/home'
+    )
     arg("--type",
         "-t",
         type=str,
@@ -57,16 +78,36 @@ if __name__ == "__main__":
     arg("--cur",
         default=get_time(),
         help="current date and time as string")
+
     args = parser.parse_args()
+
+    #### Config with yaml
+    config_args = OmegaConf.create(vars(args))
+    config_yaml = OmegaConf.load(args.config)
+
+    # args에 있는 값이 config_yaml에 있는 값보다 우선함. (단, None이 아닌 값일 경우)
+    for key in config_args.keys():
+        if config_args[key] is not None:
+            config_yaml[key] = config_args[key]
+    print("config args", args)
+    args = config_yaml
+
+    ##### setting
+    setting = Setting()
+
+    ##### logger initialize
+    logger = Logger(args, setting.get_log_path(args))
+    logger.save_args()
 
     model_type = {'g':'general_recommender','s':'sequential_recommender','c':'context_aware_recommender'}
     recbole_model = importlib.import_module('recbole.model.'+model_type.get(args.type))
     model_class = getattr(recbole_model, args.model)
 
-    config = Config(model=args.model, config_file_list=['Config/run.yaml', 'Config/setting.yaml'])
-    config['checkpoint_dir'] = os.path.join(
-        config['checkpoint_dir']+args.model+'_'+args.cur
-    )
+    config = Config(model=args.model, config_file_list=['../configs/recbole_model.yaml', '../configs/recbole_setting.yaml'])
 
+    config['checkpoint_dir'] = os.path.join(
+        args.root_dir,args.train.ckpt_dir,args.model+'_'+args.cur
+    )
+    print(config)    
     generate_data(args=args, config=config)
-    train(config=config)
+    train(config=config, logger=logger)
