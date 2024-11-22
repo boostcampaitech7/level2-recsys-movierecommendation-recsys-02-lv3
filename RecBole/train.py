fb4bbb7ad6376c2234236b56d1d096b6efdc5176
jaeeun <jjjaeunn@gmail.com>
2024-11-22 11:08:02 +0900

    style: print문 삭제

--- RecBole/train.py
+++ RecBole/train.py
@@ -6,12 +6,13 @@ from datetime import datetime
 
 from recbole.config import Config
 from recbole.trainer import Trainer
+
 import sys
 sys.path.insert(0, "/data/ephemeral/home")
 from src.utils.util import Logger, Setting
 from dataloader import generate_data, get_data
 
-# checkpoint 생성 시 모델 학습 시각 기록을 위한 함수
+
 def get_time():
     now = datetime.now()
     # 월, 일, 시간 추출
@@ -45,9 +46,11 @@ def train(config,logger):
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
+
     arg = parser.add_argument
-    # model의 종류를 정의합니다 (general, sequential, context-aware)
+
     arg("--config", 
         "-c",
         help='Configuration 파일을 설정합니다.',
@@ -62,24 +65,25 @@ if __name__ == "__main__":
         "-t",
         type=str,
         choices=['g','s','c'],
-        help="type of recommender model(g:general, s:sequential, c:context-aware)"
+        help="model의 종류를 설정합니다. g:general, s:sequential, c:context-aware)"
     )
     arg("--model",
         "-m",
         type=str,
-        help="name of recommender"
+        help="추천 모델의 이름을 설정합니다."
     )
     arg("--data_path",
         "-d",
         type=str,
-        help="path of saved data",
+        help="학습 데이터가 저장된 경로를 설정합니다.",
         default='./data/'
     )
     arg("--cur",
         default=get_time(),
-        help="current date and time as string")
+        help="현재 시간을 기록합니다.")
 
     args = parser.parse_args()
+
 
     #### Config with yaml
     config_args = OmegaConf.create(vars(args))
@@ -89,8 +93,9 @@ if __name__ == "__main__":
     for key in config_args.keys():
         if config_args[key] is not None:
             config_yaml[key] = config_args[key]
-    print("config args", args)
+ 
     args = config_yaml
+
 
     ##### setting
     setting = Setting()
@@ -103,11 +108,12 @@ if __name__ == "__main__":
     recbole_model = importlib.import_module('recbole.model.'+model_type.get(args.type))
     model_class = getattr(recbole_model, args.model)
 
+    #### set config and args
     config = Config(model=args.model, config_file_list=['../configs/recbole_model.yaml', '../configs/recbole_setting.yaml'])
 
     config['checkpoint_dir'] = os.path.join(
         args.root_dir,args.train.ckpt_dir,args.model
     )
-    print(config)    
+  
     generate_data(args=args, config=config)
     train(config=config, logger=logger)
