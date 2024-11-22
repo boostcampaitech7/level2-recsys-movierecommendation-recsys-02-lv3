32366e6e505bc89b9e53c5775ff3390e5297be3e
choitaesoon <cts0817@naver.com>
2024-11-22 11:16:13 +0900

    feat-28: Add ease_predict.py #28

--- src/trainers/inference.py
+++ src/trainers/inference.py
@@ -33,3 +33,17 @@ def multivae_predict(args, model, data, k = 10):
 
 
     return top_items
+
+
+
+def ease_predict(args, model, data, k):
+    result = model.forward(data[:,:])
+    result[data.nonzero()] = -np.inf
+    
+    top_items = []
+    _  , top_ids = torch.topk(torch.from_numpy(result).to(args.device), k, dim=1)
+    for user_id, item_ids in enumerate(top_ids):
+            for item_id in item_ids:
+                top_items.append((user_id, item_id.item()))
+
+    return top_items
