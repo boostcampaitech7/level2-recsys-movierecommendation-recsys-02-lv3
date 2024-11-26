7b59cc0aec90a16deaf158b801bfd2d42192388a
jaeeun <jjjaeunn@gmail.com>
2024-11-26 17:10:04 +0900

    feat: RecVAE inference 모듈 추가 #35

--- src/RecVAE/inference.py
+++ src/RecVAE/inference.py
@@ -4,11 +4,11 @@ import numpy as np
 
 def recvae_predict(args, model, data, k = 10):
     """
-    Multi-VAE 모델을 사용하여 각 사용자별 Top-K 추천 아이템을 추출하는 함수
+    Rec-VAE 모델을 사용하여 각 사용자별 Top-K 추천 아이템을 추출하는 함수
     
     Args:
         args: ArgumentParser에서 정의된 학습 및 환경 파라미터
-        model: 학습된 Multi-VAE 모델
+        model: 학습된 Rec-VAE 모델(체크포인트로 전달)
         data: 사용자-아이템 상호작용 데이터
         k: 추천할 아이템의 개수 (default: 10)
 
@@ -17,19 +17,17 @@ def recvae_predict(args, model, data, k = 10):
     """
     
     model.eval()
+
     top_items = []
     
     with torch.no_grad():
-        data_tensor = torch.FloatTensor(data.toarray()).to(args.device)
-        predicts = model(data_tensor)
-
-        predicts = predicts[0]
+        data_tensor = torch.Tensor(data).to(args.device)
+        predicts = model(data_tensor, calculate_loss=False)
         predicts[data.nonzero()] = -np.inf
         
-        _  , top_ids = torch.topk(predicts.to(args.device).float(), k, dim=1)
+        _  , top_ids = torch.topk(predicts.to(args.device).float(), k)
         for user_id, item_ids in enumerate(top_ids):
             for item_id in item_ids:
                 top_items.append((user_id, item_id.item()))
-
 
     return top_items
